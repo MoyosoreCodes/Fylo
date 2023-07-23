011d21b55c8259d5a7a42733f2e8f251df640bcc
Moyo <31387340+MoyosoreCodes@users.noreply.github.com>
2023-07-23 11:46:44 +0100

    Added background color and header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,25 @@
+import "./Header.css";
+import Logo from "../svg/Logo";
+import { useState } from "react";
+export default function Header() {
+  const [links, setLinks] = useState([
+    { name: "features" },
+    { name: "team" },
+    { name: "sign in" },
+  ]);
+
+  const displayLinks = links.map((link, index) => {
+    return <li key={index}> {link.name} </li>;
+  });
+  return (
+    <header className="header-ctn">
+      <div className="logo-ctn">
+        <Logo />
+      </div>
+
+      <div className="actions">
+        <ul>{displayLinks}</ul>
+      </div>
+    </header>
+  );
+}
